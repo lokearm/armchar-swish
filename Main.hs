bc628d2939df9a6c4a2227d95d5446bc15c0b679
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2022-04-17 10:07:16 +0200

    Drafting and testing

--- Main.hs
+++ Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+import System.IO
+import Control.Monad
+
+import Swish.RDF.Ruleset
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+        let list = []
+        handle <- openFile "Ontology/cieran.ttl" ReadMode
+        contents <- hGetContents handle
+        print contents
+        hClose handle
+
+f :: [String] -> [Int]
+f = map read
+
