896982b5abba27bc634a22f19cb194ae3bee7017
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2022-04-17 10:29:16 +0200

    Testing Turtle parser

--- Main.hs
+++ Main.hs
@@ -2,17 +2,29 @@ module Main where
 
 import System.IO
 import Control.Monad
+import Swish.RDF.Parser.Turtle
+import Swish.RDF.Formatter.Turtle
+import Data.Text.Lazy
+
+
+
 
 import Swish.RDF.Ruleset
+
+armFile = "Ontology/arm.ttl"
+resourceFile = "Ontology/resources.ttl"
+characterFile = "Ontology/cieran.ttl"
+baseURI = Nothing
+
 
 main :: IO ()
 main = do
         let list = []
-        handle <- openFile "Ontology/cieran.ttl" ReadMode
+        handle <- openFile characterFile ReadMode
         contents <- hGetContents handle
-        print contents
+        let character = parseTurtle ( pack contents ) baseURI 
+        case (character) of
+           (Left s) -> print s
+           (Right c) -> print $ formatGraphAsText c
         hClose handle
-
-f :: [String] -> [Int]
-f = map read
 
