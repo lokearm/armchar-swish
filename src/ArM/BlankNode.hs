dbfbe0439f9073ecf6d2ea61d1be7f1a859552b6
Hans Georg Schaathun <georg@schaathun.net>
2022-04-21 17:42:17 +0200

    missing file

---
+++ src/ArM/BlankNode.hs
@@ -0,0 +1,15 @@
+
+module ArM.BlankNode where
+
+import Swish.RDF.Graph
+import Control.Monad.State.Lazy
+
+type BlankState = State (String,Int) 
+
+getBlank :: BlankState RDFLabel
+getBlank = do
+    (s,x) <- get
+    put (s,x+1)
+    return $ Blank $ s ++ show x
+
+runBlank = runState
