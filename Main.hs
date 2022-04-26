5628d492997d4c68dcbcde844ca6c7172aa3a24b
Hans Georg Schaathun <georg@schaathun.net>
2022-04-26 10:14:01 +0200

    qualified imports in Main

--- Main.hs
+++ Main.hs
@@ -4,13 +4,14 @@
 module Main where
 
 import System.IO as IO
-import Control.Monad
-import qualified Data.Text.Lazy as  T
-import Swish.RDF.Formatter.Turtle
-import Swish.RDF.Graph
-import Data.Maybe
-import Data.Text (Text)
+import Control.Monad (join)
+import qualified Swish.RDF.Formatter.Turtle as TTL
+import Swish.RDF.Graph (merge)
+import Data.Maybe (fromJust)
 import Control.Monad.IO.Class (liftIO)
+--
+-- import qualified Data.Text.Lazy as  T
+-- import Data.Text (Text)
 
 import Data.Aeson.Encode.Pretty (encodePretty)
 import qualified Data.ByteString.Lazy.Char8 as B
@@ -19,19 +20,18 @@ import qualified Control.Concurrent.STM as STM
 
 -- Web service
 import qualified Web.Scotty  as S
-import ArM.WebService
+import ArM.WebService (stateScotty)
 
 -- Auth
-import Network.Wai.Middleware.HttpAuth
+-- import qualified Network.Wai.Middleware.HttpAuth as HA
 import Data.SecureMem -- for constant-time comparison
 
 -- Timer
-import System.CPUTime
 import ArM.Time
 
-import ArM.Load
-import ArM.Resources
-import ArM.Character.Character as C
+import ArM.Load (getGraph)
+import qualified ArM.Resources as AR
+import qualified ArM.Character.Character as C
 import qualified ArM.CharacterQuery as CQ
 import qualified ArM.CharacterMap as CM
 
@@ -41,16 +41,16 @@ password :: SecureMem
 password = secureMemFromByteString "ElksRun" 
 
 
-testCharacter = armcharRes "cieran"
+testCharacter = AR.armcharRes "cieran"
 
 -- main :: IO ()
 main = do 
-     (g,schema,res) <- getGraph characterFile armFile resourceFile
+     (g,schema,res) <- getGraph AR.characterFile AR.armFile AR.resourceFile
 
 
-     print $ formatGraphAsText $ schema
-     print $ formatGraphAsText $ res
-     print $ formatGraphAsText $ g
+     print $ TTL.formatGraphAsText $ schema
+     print $ TTL.formatGraphAsText $ res
+     print $ TTL.formatGraphAsText $ g
 
      let s = merge schema res
      let cl =  fromJust $ C.getAllCS g testCharacter
@@ -58,7 +58,7 @@ main = do
      -- let cmap = CM.insertList CM.empty $ cl
      stateVar <- STM.newTVarIO CM.MapState { CM.stMap = cmap }
 
-     print $ getGameStartCharacter g $ armcharRes "cieran" 
+     print $ C.getGameStartCharacter g $ testCharacter
      printTime
 
      -- print $ encodePretty $ A.getIngameAdvancements g testCharacter
@@ -75,4 +75,4 @@ main = do
      print "Starting Scotty"
      S.scotty 3000 $ stateScotty g schema res stateVar
 
-        -- middleware $ basicAuth authf "armchar"
+        -- HA.middleware $ basicAuth authf "armchar"
