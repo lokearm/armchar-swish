a0b44f4644a2db15ce56e2f88495a25f69aa624f
Hans Georg Schaathun <georg@schaathun.net>
2022-04-27 16:05:03 +0200

    Parallel map

--- src/ArM/Rules/Schema.hs
+++ src/ArM/Rules/Schema.hs
@@ -23,6 +23,8 @@ import ArM.Rules.Aux
 import ArM.Rules.RDFS
 import Swish.VarBinding (varBindingId) 
 
+import Control.Parallel.Strategies
+
 -- | Simple forward application of a rule
 -- When this results in multiple graphs, this are added together
 -- using 'addGraphs' (via 'foldGraphs')
@@ -30,7 +32,7 @@ fwdApplySimpleS :: RDFGraph -> RDFRule ->  RDFGraph -> RDFGraph
 fwdApplySimpleS schema r cg = foldGraphs $ fwdApply r [schema,cg]
 
 fwdApplyListS :: RDFGraph -> [RDFRule] ->  RDFGraph -> RDFGraph
-fwdApplyListS schema rs g = foldl addGraphs g $ map (`f` g) rs
+fwdApplyListS schema rs g = foldl addGraphs g $ parMap rpar (`f` g) rs
      where f = fwdApplySimpleS schema
 fwdApplyListSR :: RDFGraph -> [RDFRule] ->  RDFGraph -> RDFGraph
 fwdApplyListSR schema rs g = if (g' == g) then g'
