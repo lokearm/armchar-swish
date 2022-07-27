31ee025427cb91241a0941edcf46899b40223875
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2022-07-27 22:05:53 +0200

    Some more features in the client character sheet.

--- client/client.py
+++ client/client.py
@@ -3,13 +3,16 @@
 import http.client, urllib.parse
 import json
 
-conn = http.client.HTTPConnection("localhost:3000")
-conn.request("GET", "/ability/cieran/1217/Autumn" )
+def get(conn,path):
+   conn.request("GET", path)
+   response = conn.getresponse()
+   print(path,response.status, response.reason)
+   data = response.read()
+   return json.loads(data)
 
-response = conn.getresponse()
-print(response.status, response.reason)
-data = response.read()
-y = json.loads(data)
+conn = http.client.HTTPConnection("localhost:3000")
+
+y = get(conn,"/ability/cieran/1217/Autumn" )
 
 ab = [ (i.get("arm:hasLabel","???"),i.get("arm:hasSpeciality","-"),i.get("arm:hasScore","-"),i.get("arm:hasXP","-")) for i in y ]
 ab.sort()
@@ -21,16 +24,26 @@ for i in ab:
     output.append( f"  \\Anability[{i[1]}]{{{i[0]}}}{{{i[2]}}}{{{i[3]}}}" )
 output.append( "\\end{abilities}" )
 
+
+y = get(conn,"/virtue/cieran/1217/Autumn" )
+y += get(conn,"/flaw/cieran/1217/Autumn" )
+
+output.append( "\\begin{vf}" )
+for i in y:
+    output.append( f"  \\vfLine{{{i.get('arm:hasLabel','???')}}}{{{i.get('arm:hasScore','?')}}}" )
+output.append( "\\end{vf}" )
+
+y = get(conn,"/pt/cieran/1217/Autumn" )
+output.append( "\\begin{personality}" )
+for i in y:
+    output.append( f"  \\aPtrait{{{i.get('arm:hasLabel','???')}}}{{{i.get('arm:hasScore','?')}}}" )
+output.append( "\\end{personality}" )
+
 output.append(  "\\end{magus}" )
-
-conn.request("GET", "/virtue/cieran/1217/Autumn" )
-
-response = conn.getresponse()
-print(response.status, response.reason)
-data = response.read()
-y = json.loads(data)
-
 f = open("magus.tex", "w")
 for line in output:
     f.write(line+"\n")
 f.close()
+
+# http get :3000/art/cieran/1217/Summer
+# http get :3000/characteristic/cieran/1217/Summer
