c55e54868d3c269c7a77869263fa93f1b285790d
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2022-07-27 21:30:01 +0200

    More work on the client

--- client/client.py
+++ client/client.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import http.client, urllib.parse
 import json
 
@@ -7,21 +9,28 @@ conn.request("GET", "/ability/cieran/1217/Autumn" )
 response = conn.getresponse()
 print(response.status, response.reason)
 data = response.read()
-print(data)
 y = json.loads(data)
 
 ab = [ (i.get("arm:hasLabel","???"),i.get("arm:hasSpeciality","-"),i.get("arm:hasScore","-"),i.get("arm:hasXP","-")) for i in y ]
 ab.sort()
 
+output = [ "\documentclass{armsheet}", "\\begin{magus}" ]
+
+output.append( "\\begin{abilities}" )
 for i in ab:
-    print( i )
+    output.append( f"  \\Anability[{i[1]}]{{{i[0]}}}{{{i[2]}}}{{{i[3]}}}" )
+output.append( "\\end{abilities}" )
+
+output.append(  "\\end{magus}" )
 
 conn.request("GET", "/virtue/cieran/1217/Autumn" )
 
 response = conn.getresponse()
 print(response.status, response.reason)
 data = response.read()
-print(data)
 y = json.loads(data)
-for i in y:
-    print( i )
+
+f = open("magus.tex", "w")
+for line in output:
+    f.write(line+"\n")
+f.close()
