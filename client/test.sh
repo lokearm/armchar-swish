2e27ef0f68cdf08a800d114462fcf91baec33efb
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2022-07-29 08:22:05 +0200

    Test against baseline files.

--- client/test.sh
+++ client/test.sh
@@ -1,6 +1,11 @@
 
 export TEXINPUTS=.:texinputs/:
+
 python3 client.py
-pdflatex magus
 python3 adv.py
-pdflatex adv
+
+diff baseline-adv.tex adv.tex && echo "character sheet is OK"
+diff baseline-magus.tex magus.tex && echo "advancement log is OK"
+
+# pdflatex magus
+# pdflatex adv
