0396d1003f8ab10578b3d43c63d63d07b2cb1544
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2022-07-29 08:12:31 +0200

    Refactored Rules.Aux and removed unnecessary imports

--- src/ArM/Rules/Aux.hs
+++ src/ArM/Rules/Aux.hs
@@ -57,13 +57,6 @@ fwdApplyListR rs g = if (g' == g) then g'
                      where g' = fwdApplyList rs g
 
 
--- | Convenience function to make a Rule from Strings of Notation3 data.
-makeRule ln s1 s2 = makeN3ClosureSimpleRule 
-    rulesNS ( newLName ln ) ( fromString s1 ) ( fromString s2 )
-
-makeRule' ln s1 s2 = makeRDFClosureRule (makeSN ln) [g1] g2 varBindingId
-   where g1 = makeRDFGraphFromN3Builder $ fromString $ prefixes ++ s1
-         g2 = makeRDFGraphFromN3Builder $ fromString $ prefixes ++ s2
 
 typeRes = Res rdfType 
 subclassRes = Res rdfsSubClassOf 
@@ -91,13 +84,3 @@ makeCRule s l1 l2 = makeRDFClosureRule ( makeSN s )
             [listToRDFGraph  l1]
             (listToRDFGraph  l2)
             varBindingId
-
-prefixes = "@prefix owl: <http://www.w3.org/2002/07/owl#> . "
-   ++ "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> . "
-   ++ "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> . "
-   ++ "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> . "
-   ++ "@prefix foaf: <http://xmlns.com/foaf/0.1/>. "
-   ++ "@prefix dc: <http://purl.org/dc/elements/1.1/> . "
-   ++ "@prefix arm: <https://hg.schaathun.net/armchar/schema#> . "
-   ++ "@prefix armr: <https://hg.schaathun.net/armchar/resources#> . "
-   ++ "@prefix armchar: <https://hg.schaathun.net/armchar/character/> . "
--- src/ArM/Rules/RDFS.hs
+++ src/ArM/Rules/RDFS.hs
@@ -13,33 +13,34 @@
 
 module ArM.Rules.RDFS where
 
-import Swish.RDF.Ruleset
-import qualified Data.Text as T
-import Swish.Rule
 import Swish.RDF.Graph
-import ArM.Resources
 import ArM.Rules.Aux
+import Swish.RDF.Vocabulary.RDF
 
--- | RDFS Rules
+-- | Infer subclass and subproperty relations by transitivity
 rdfsRules = [
-    makeRule "subclassRule" 
-       "?s rdfs:subClassOf ?t . ?t rdfs:subClassOf ?c ."
-       "?s rdfs:subClassOf ?c ."
-    , makeRule "subpropRule" 
-       "?s rdfs:subPropertyOf ?t . ?t rdfs:subPropertyOf ?c ."
-       "?s rdfs:subPropertyOf ?c ."
-       ]
+  makeCRule "subclassRule" 
+      [ arc sVar (Res rdfsSubClassOf) tVar 
+      , arc tVar (Res rdfsSubClassOf) cVar ]
+      [ arc sVar (Res rdfsSubClassOf) cVar ]
+  , makeCRule "subpropRule" 
+      [ arc sVar (Res rdfsSubPropertyOf) tVar 
+      , arc tVar (Res rdfsSubPropertyOf) cVar ]
+      [ arc sVar (Res rdfsSubPropertyOf) cVar ]
+  ]
 
 -- | Rules to infer additional types and properties from subclass 
 -- and subproperty relations (using RDFS vocabulary).
 rdfstypeRules = [
-    makeRule "subclasstypeRule"
-       "?s rdf:type ?t . ?t rdfs:subClassOf ?c ."
-       "?s rdf:type ?c ."
-    , makeRule "subpropinstanceRule" 
-       "?s ?p ?o . ?p rdfs:subPropertyOf ?p2 ."
-       "?s ?p2 ?o ."
-       ]
+    makeCRule "subclasstypeRule"
+      [ arc sVar (Res rdfType) tVar 
+      , arc tVar (Res rdfsSubClassOf) cVar ]
+      [ arc sVar (Res rdfType) cVar ]
+    , makeCRule "subpropinstanceRule" 
+      [ arc sVar pVar oVar 
+      , arc pVar (Res rdfsSubPropertyOf) (Var "p2") ] 
+      [ arc sVar (Var "p2") oVar ]
+    ]
 
 -- | Make inferences on the joint graph including resources
 applyRDFS = fwdApplyList rdfstypeRules . fwdApplyListR rdfsRules 
