fdb2f0b55af8ba2fa6fb4f2ad68945b5e191fd1a
Hans Georg Schaathun <georg@schaathun.net>
2022-08-11 13:12:17 +0200

    Removed redundant imports

--- src/Main.hs
+++ src/Main.hs
@@ -12,59 +12,43 @@
 -- other modules, and some configuration options which may later be
 -- moved to data files or command line parameters.
 --
+-- + The data structure is loaded by the `getRawGraph` function
+--   defined in `ArM.Load`
+-- + Software Transactional Memory is set up by the `getState` function
+--   from `ArM.STM`.
+-- + The Web API is defined in the `ArM.WebServices` module.
+--
 -----------------------------------------------------------------------------
 
 module Main where
 
-import System.IO as IO
-import Control.Monad (join)
-import qualified Swish.RDF.Formatter.Turtle as TTL
-import Swish.RDF.Graph (merge,fromRDFLabel)
-import Data.Maybe (fromJust)
-import Control.Monad.IO.Class (liftIO)
+import System.IO (IO)
+
+-- Loading ArM data 
+import ArM.Load (getRawGraph)
+import qualified ArM.Resources as AR
+
+-- Software Transactional Memory
 import qualified Control.Concurrent.STM as STM
-
--- import qualified Data.Text.Lazy as  T
--- import Data.Text (Text)
-
-import ArM.Character.Metadata (characterFromGraph)
-
-import Data.Aeson.Encode.Pretty (encodePretty)
-import qualified Data.ByteString.Lazy.Char8 as B
-
--- import qualified Control.Concurrent.STM as STM
 import ArM.STM
 
 -- Web service
 import qualified Web.Scotty  as S
 import ArM.WebService (stateScotty)
 
-import Swish.Namespace (ScopedName,getScopeLocal)
-
--- Auth
+-- Authentication
 -- import qualified Network.Wai.Middleware.HttpAuth as HA
 import Data.SecureMem -- for constant-time comparison
-import qualified Network.URI as URI
 
 -- Timer
 import ArM.Time
 
-import ArM.Load (getRawGraph)
-import qualified ArM.Resources as AR
-import qualified ArM.Character.Character as C
-import qualified ArM.CharacterQuery as CQ
-
+-- Authentication
 authf u p = return $ u == "user" && secureMemFromByteString p == password
 password :: SecureMem
 password = secureMemFromByteString "ElksRun" 
 
-testCharacter = AR.armcharRes "cieran"
-
-
-local Nothing = "Nothing"
-local (Just x) = getScopeLocal x
-
--- main :: IO ()
+main :: IO ()
 main = do 
      print "Starting: armchar-swish  ..."
      printTime
@@ -75,7 +59,6 @@ main = do
            do print $ "Error: " ++ x
         Left x -> do
            stateVar <- STM.newTVarIO x
-           let charlabel = characterFromGraph g
            print $ "Loaded character: " ++ show (characterLabel x)
            print $ "Character ID: " ++ (characterID x)
 
