d42e81bf8cdd767f8d08d48444e94103eefec895
Hans Georg Schaathun <georg@schaathun.net>
2022-08-15 19:43:31 +0200

    docstring

--- src/ArM/Rules/FullGraph.hs
+++ src/ArM/Rules/FullGraph.hs
@@ -11,9 +11,8 @@
 -- Rules used include
 -- 1.  String properties are added to avoid having to handle simple objects
 --     at the client.
--- 2.  Infer RDF types from the arm:traitClass property.
---     Trait and Possession instances has a unique arm:traitClass property
---     which defines the class to which they directly belong.
+-- 2.  Infer Trait and Possession properties from their class
+--     (via the `arm:traitClass` property).
 -- 3.  Infer cost (score) of virtues and flaws.
 -- 4.  Infer that advancements advance traits granted by virtues and flaws
 -- 5.  Infer special traits.
