89d2b338f82364ac979cf3a27ad16f2bb5bf7afb
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2022-08-30 08:19:57 +0200

    docstring

--- src/ArM/NoTrace.hs
+++ src/ArM/NoTrace.hs
@@ -1,3 +1,16 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  ArM.NoTrace
+-- Copyright   :  (c) Hans Georg Schaathun <hg+gamer@schaathun.net>
+-- License     :  see LICENSE
+--
+-- Maintainer  :  hg+gamer@schaathun.net
+--
+-- This module defines the same functions as `ArM.Trace` to be identity
+-- functions; thus the debug trace can be turned off simply by importing
+-- this module instead of `ArM.Trace`, leaving the debug code in place.
+--
+-----------------------------------------------------------------------------
 
 module ArM.NoTrace where
 
