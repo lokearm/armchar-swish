75db3673156188503e615035adeefb621c1e5843
Hans Georg Schaathun <georg@schaathun.net>
2023-10-29 09:49:10 +0100

    Remove STM from CLI tool

--- src/Main.hs
+++ src/Main.hs
@@ -14,24 +14,13 @@
 --
 -- * The data structure is loaded by the `getRawGraph` function
 --   defined in `ArM.Load`
--- * Software Transactional Memory is set up by the `getState` function
---   from `ArM.STM`.
--- * The Web API is defined in the `ArM.WebServices` module.
 --
 -----------------------------------------------------------------------------
 
 module Main where
 
--- Software Transactional Memory
-import ArM.STM
-
 -- Timer
 import ArM.Time
-
--- Authentication
-
--- | The `authf` function validates the password in the Wai middleware
--- authf u p = return $ u == "user" && secureMemFromByteString p == password
 
 -- | Saga File
 sagaFile :: String
@@ -41,5 +30,4 @@ main :: IO ()
 main = do 
      putStrLn "Starting: armchar-swish  ..."
      printTime
-     stateVar <- loadSaga sagaFile
      putStrLn "Not implemented"
