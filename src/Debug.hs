9da7e3e24443c045361aff2879d5e9bea407322a
Hans Georg Schaathun <georg@schaathun.net>
2023-10-30 20:24:42 +0100

    new debug program

---
+++ src/Debug.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Main
+-- Copyright   :  (c) 2023: Hans Georg Schaathun <hg+gamer@schaathun.net>
+-- License     :  see LICENSE
+--
+-- Maintainer  :  hg+gamer@schaathun.net
+--
+-----------------------------------------------------------------------------
+
+module Main where
+
+-- Timer
+import ArM.Time
+import ArM.CharGraph
+import ArM.Markdown.CharacterSheet
+import ArM.Character.CharGen
+-- import ArM.Types.Character
+
+-- | Saga File
+sagaFile :: String
+sagaFile = "Test/saga.ttl"
+charFile :: String
+charFile = "Test/cieran.ttl"
+
+main :: IO ()
+main = do 
+     putStrLn "Starting: armchar-swish  ..."
+     printTime
+     sagaobject <- loadSaga sagaFile
+     chargen <- loadChar sagaobject charFile
+     let char = head $ charSheets chargen
+     let chargraph = sheetGraph char
+
+     putStrLn $ show chargraph
