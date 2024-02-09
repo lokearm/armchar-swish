6e7f3bf75b00862373f8f91c8c812c846205bee8
Hans Georg Schaathun <georg@schaathun.net>
2024-02-09 23:23:17 +0100

    cleanup of cabal file and advancement log output

--- src/ArM/Markdown/AdvancementLog.hs
+++ src/ArM/Markdown/AdvancementLog.hs
@@ -15,12 +15,25 @@ module ArM.Markdown.AdvancementLog ( printAdvancementLog ) where
 
 
 import ArM.Types.Advancement
+import ArM.Types.Season
 import Data.List(intercalate)
+import Data.Maybe (fromJust)
 
 
 printAdvancementLog :: [Advancement] -> [String]
-printAdvancementLog = intercalate [] . map printAdvancement
+printAdvancementLog = intercalate [] . map printAdvancement . filterAdv
+
+advYear :: Advancement -> Maybe Int
+advYear = charYear . advTime 
+
+filterAdv :: [Advancement] -> [Advancement]
+filterAdv []  = []
+filterAdv (x:xs) | advYear x == Nothing = filterAdv xs 
+filterAdv (x:xs) | otherwise = x:filterAdv xs 
 
 printAdvancement :: Advancement -> [String]
-printAdvancement ad = [ "+ " ++ (show . advTime) ad ]
+printAdvancement ad = [ "+ " ++ showSeason ad ]
+
+showSeason :: Advancement -> String 
+showSeason a =  ( charSeason . advTime ) a ++ " " ++ ( show . fromJust . advYear ) a 
 
