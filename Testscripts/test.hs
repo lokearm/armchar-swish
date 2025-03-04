3ef1e06f25ff2e9a9e6a4bccf015d0ee592e57eb
Hans Georg Schaathun <georg@schaathun.net>
2025-03-04 14:02:41 +0100

    test script

---
+++ Testscripts/test.hs
@@ -0,0 +1,23 @@
+
+import ArM.Char.Virtues
+import ArM.Char.Trait
+import qualified Data.Map as Map
+
+k = VFKey "Puissant (art)"
+
+
+-- ghci> :t lookup
+-- lookup :: Eq a => a -> [(a, b)] -> Maybe b
+-- ghci> % :t Map.lookup
+-- Map.lookup :: Ord k => k -> Map.Map k a -> Maybe a
+
+v = VF "Puissant (art)" "Creo" 5 Nothing
+
+-- ghci> vv
+-- VF {vfname = "Puissant (art)", vfDetail = "Creo", vfcost = 5, vfAppliesTo = Nothing}
+
+f = fromJust (Map.lookup k virtueMap )
+
+-- ghci> f vv
+-- Art: Creo 0xp
+
