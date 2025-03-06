959498efb3ed02a3140a5c4a4a5c9696175733d3
Hans Georg Schaathun <georg@schaathun.net>
2025-03-06 01:55:28 +0100

    Markdown compiles

--- src/ArM/Char/Markdown.hs
+++ src/ArM/Char/Markdown.hs
@@ -14,6 +14,7 @@ module ArM.Char.Markdown (printMD) where
 import Data.Maybe (fromJust)
 import ArM.Char.Character 
 import ArM.Char.Trait
+import ArM.Char.Advancement
 
 class Markdown a where
      printMD :: a -> [ String ]
@@ -29,13 +30,16 @@ instance Markdown CharacterConcept where
    printMD c = ("# " ++ fullConceptName c ):"": 
       ( printMD $ charGlance c ) ++ ( printMD $ charData c )
 
+listMD :: Markdown a => [a] -> [String]
+listMD = foldl (++) [] . map printMD 
+
 instance Markdown Character where
    printMD c = ( printMD . concept ) c 
             ++ maybeP (state c)
-            ++ ("":"## Pregame Development":"":xf as)
-            ++ ("":"## Past Advancement":"":xf bs)
-            ++ ("":"## Future Advancement":"":xf cs)
-       where xf = foldl (++) [] . map printMD 
+            ++ ("":"## Pregame Development":"":listMD as)
+            ++ ("":"## Past Advancement":"":listMD bs)
+            ++ ("":"## Future Advancement":"":listMD cs)
+       where 
              as = pregameAdvancement c
              bs = pastAdvancement c
              cs = futureAdvancement c
@@ -45,6 +49,20 @@ instance Markdown Character where
 instance Markdown CharacterState where
    printMD c = ( "## " ++ show (charTime c) ):"":pt c
        where pt = map ("+ "++) . foldl (++) [] . map printMD . traits 
+instance Markdown AugmentedAdvancement where
+   printMD a = f (season a) (mode a) $ fn (narrative a) $ pt a
+      where xps | sx == Nothing = ""
+                | otherwise = " (" ++ ishow  sx ++ "xp)" 
+            sx = sourceQuality a
+            ishow = show . fromJust
+            pt = map ("    + "++) . foldl (++) [] . map printMD . changes 
+
+            fn Nothing xs = xs
+            fn (Just x) xs = ( "    + " ++ show ( x ) ) :xs
+            f Nothing Nothing xs = ("+ ?? " ++ xps):xs
+            f (Just x) Nothing xs = ("+ " ++ x ++ xps):xs
+            f Nothing (Just x) xs = ("+ " ++ x ++ xps):xs
+            f (Just x) (Just y) xs = ("+ " ++ x ++ xps):("    + " ++ y):xs
 instance Markdown Advancement where
    printMD a = f (season a) (mode a) $ fn (narrative a) $ pt a
       where xps | sx == Nothing = ""
