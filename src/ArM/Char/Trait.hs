50c03b6445da41e2592d5b3fa0ec7b37462054f1
Hans Georg Schaathun <georg@schaathun.net>
2025-03-10 09:32:43 +0100

    Removed legacy code for advancing ProtoTrait objects

--- src/ArM/Char/Trait.hs
+++ src/ArM/Char/Trait.hs
@@ -582,17 +582,6 @@ instance TraitLike ProtoTrait where
        | other p /= Nothing = OtherTraitKey $ fromJust $ other p
        | otherwise  = error "No Trait for this ProtoTrait" 
 
-   advanceTrait a
-       | ability a /= Nothing = updateSpec a . updateXP a
-       | characteristic a /= Nothing = updateScore a . updateAging a
-       | art a /= Nothing = updateXP a 
-       | spell a /= Nothing = updateXP a . updateMastery a
-       | ptrait a /= Nothing = updateScore a 
-       | reputation a /= Nothing = updateScore a 
-       | confidence a /= Nothing = updateScore a . updatePts a
-       | other a /= Nothing = updatePts a
-       | otherwise  = id
-     where updatePts ad t = t { points = maybeAdd ( points t ) ( points ad ) }
    toTrait p 
       | ability p /= Nothing = AbilityTrait $ fromJust $ computeTrait p
       | characteristic p /= Nothing = CharacteristicTrait $  fromJust $ computeTrait p
@@ -645,24 +634,6 @@ updateSpellXP x ab | x < tr = ab { spellExcessXP = x }
 updateSpellMastery :: [String] -> Spell -> Spell
 updateSpellMastery ms t = t { masteryOptions = (masteryOptions t) ++ ms }
 
-updateSpec :: ProtoTrait -> ProtoTrait -> ProtoTrait
-updateSpec a = u (spec a)
-    where u Nothing t = t
-          u (Just x) t = t { spec = Just x }
-updateScore :: ProtoTrait -> ProtoTrait -> ProtoTrait
-updateScore a = u (score a)
-    where u Nothing t = t
-          u (Just x) t = t { score = Just x }
-updateXP :: ProtoTrait -> ProtoTrait -> ProtoTrait
-updateXP a t = t { xp = maybeAdd (xp a) (xp t) }
-
-updateAging :: ProtoTrait -> ProtoTrait -> ProtoTrait
-updateAging a t = t { aging = maybeAdd ( aging t ) ( aging a ) }
-updateMastery :: ProtoTrait -> ProtoTrait -> ProtoTrait
-updateMastery a t = t { mastery = f (mastery t) (mastery a) }
-    where f Nothing x = x
-          f y Nothing = y
-          f (Just x) (Just y)  = Just (x ++ y)
 
 -- | Apply a list of ProtoTrait advancements to a list of Traits.
 advance :: [ ProtoTrait ] -> [ Trait ] -> [ Trait ]
