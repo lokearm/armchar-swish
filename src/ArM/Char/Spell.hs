0c9f9350715a7582983df2378b875c8845f83581
Hans Georg Schaathun <georg@schaathun.net>
2025-03-13 08:15:40 +0100

    Created the spellDB function.

--- src/ArM/Char/Spell.hs
+++ src/ArM/Char/Spell.hs
@@ -14,37 +14,40 @@
 --
 -----------------------------------------------------------------------------
 module ArM.Char.Spell ( SpellRecord(..)
-                       , fromCSVline
+                       , spellDB
                        ) where
 
 import ArM.Char.Trait
 import GHC.Generics
 import Data.List.Split
+import qualified Data.Map as M
 
 data SpellRecord = SpellRecord
-                   { spellKey :: TraitKey
-                   , lvl :: Int
+                   { spellKey :: TraitKey   -- ^ Unique identifier as used in `ArM.Char.Trait`
+                   , lvl :: Maybe Int       -- ^ Spell Level.  General Level Spells have Nothing.
                    , technique :: String
                    , techniqueReq :: [String]
                    , form :: String
                    , formReq :: [String]
-                   , trd :: (String,String,String)
-                   , specialSpell :: [String]
-                   , description :: String
-                   , design :: String 
-                   , comment :: String 
-                   , cite :: String 
+                   , rdt :: (String,String,String)   -- ^ Range/Duration/Target
+                   , specialSpell :: [String]        -- ^ Special tags, like Ritual or Mutantum.
+                   , description :: String           -- ^ Freeform description of the effect
+                   , design :: String                -- ^ Level calculation
+                   , comment :: String               -- ^ Freeform remarks that do not fit elsewhere
+                   , cite :: String                  -- ^ Source reference
                    }
            deriving (Ord, Eq, Generic)
+
+-- | Default SpellRecord object as a starting point for step-by-step construction.
 defaultSR :: SpellRecord
 defaultSR = SpellRecord
                    { spellKey = OtherTraitKey "None"
-                   , lvl = 0
+                   , lvl = Nothing
                    , technique = ""
                    , techniqueReq = []
                    , form = ""
                    , formReq = []
-                   , trd = ("Per","Mom","Ind")
+                   , rdt = ("Per","Mom","Ind")
                    , specialSpell = []
                    , description = ""
                    , design = ""
@@ -52,6 +55,12 @@ defaultSR = SpellRecord
                    , cite = ""
                    }
 
+-- | Create a `Data.Map.Map` of SpellRecord objects.  
+-- The input is the output from `Data.CSV.csvFile`
+spellDB :: [[String]] -> M.Map TraitKey SpellRecord
+spellDB = M.fromList . map ( \ x -> (spellKey x,x) ) . map fromCSVline
+
+-- | Parse the cells of one line from the CSV file into a SpellRecord object.
 fromCSVline :: [String] -> SpellRecord
 fromCSVline (x1:x2:x3:x4:x5:x6:x7:x8:x9:x10:x11:x12:_) =
       defaultSR { spellKey = SpellKey x1 x2
