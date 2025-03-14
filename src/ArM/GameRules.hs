aea6d99ebac5501c7486ed34aa15f74817792be9
Hans Georg Schaathun <georg@schaathun.net>
2025-03-14 20:11:12 +0100

    Added docstring from feature/fractionalxp

--- src/ArM/GameRules.hs
+++ src/ArM/GameRules.hs
@@ -16,21 +16,34 @@ module ArM.GameRules where
 
 import Data.Maybe
 
-type XPType = Int
+-- | The XPType is the taype of XP.
+-- By RAW it should be Int, but some troupes prefer to count
+-- fractional XP when affinities are involved.  This requires
+-- changing to a floating point type.
+type XPType = Float
+-- type XPType = Int
+
+-- | Round the XPType if required.  
+-- This has to be redefined depending on the type of XPType.
+xpround :: Float -> XPType
+xpround = id
+-- xpround = round
+
 
 -- | Calculate score from total XP, using the arts scale.
 -- For abilities, the argument should be divided by 5 beforehand.
 scoreFromXP :: XPType -> Int
 scoreFromXP y = floor $ (-1+sqrt (1+8*x))/2
-    where x = fromIntegral y  :: Double
+    where x = y
+          -- x = fromIntegral y  :: Double
 
 pyramidScore :: Int -> XPType
-pyramidScore y | y < 0 = y*(-y+1) `div` 2
-               | otherwise = y*(y+1) `div` 2
+pyramidScore = fromIntegral . f
+  where  f y | y < 0 = y*(-y+1) `div` 2
+             | otherwise = y*(y+1) `div` 2
 
 getAbilityScore :: Maybe XPType -> (Int,XPType)
 getAbilityScore x' = (s,y) 
      where y = x - 5*pyramidScore s
            s = scoreFromXP (x `div` 5)
            x = fromMaybe 0 x'
-
