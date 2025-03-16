4ef6bcc3f57de981a4ffabf4c869841dbd3de52e
Hans Georg Schaathun <georg@schaathun.net>
2025-03-16 13:37:08 +0100

    Fixed the level cap check

--- src/ArM/Char/Trait.hs
+++ src/ArM/Char/Trait.hs
@@ -49,7 +49,7 @@ import ArM.Helper
 
 import GHC.Generics
 import Data.Aeson
-import Data.Maybe (fromJust,isNothing,fromMaybe)
+import Data.Maybe 
 import Data.List (sortBy)
 
 import ArM.Debug.Trace
@@ -592,11 +592,13 @@ instance TraitLike Art where
     toTrait = ArtTrait
     advanceTrait a x = 
           updateArtBonus (bonusScore a) $ um (multiplyXP a) $ 
-          updateArtXP y x 
+          updateArtXP lim y x 
       where y = calcXP m (artExcessXP x) (xp a) 
             m = artMultiplier x
             um Nothing ab = ab 
             um abm ar = ar { artMultiplier = fromMaybe 1.0 abm }
+            lim = levelCap a
+
 instance TraitLike Spell where
     traitKey x = SpellKey (spellFoTe x) (spellLevel x) (spellName x ) 
     toTrait = SpellTrait
@@ -679,11 +681,14 @@ updateRepXP x ab | x < tr = ab { repExcessXP = x }
     where sc = repScore ab
           tr = fromIntegral $ (sc+1)*5
 
-updateArtXP :: XPType -> Art -> Art
-updateArtXP x ab | x < tr = ab { artExcessXP = x }
-                 | otherwise = updateArtXP (x-tr) $ ab { artScore = sc+1 }
-    where sc = artScore ab
-          tr = fromIntegral (sc+1)
+updateArtXP :: Maybe Int ->  XPType -> Art -> Art
+updateArtXP lim x ab
+    | isJust lim && fromJust lim <= artScore ab = trace (show lim) $ ab
+    | x < tr = ab { artExcessXP = x }
+    | otherwise = updateArtXP lim (x-tr) $ ab { artScore = sc+1 }
+   where sc = artScore ab
+         tr = fromIntegral (sc+1)
+
 
 updateSpellXP :: XPType -> Spell -> Spell
 updateSpellXP x ab | x < tr = ab { spellExcessXP = x }
