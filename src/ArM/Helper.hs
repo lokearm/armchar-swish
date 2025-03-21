0c19458a9b5072f78ef3d44496c48fa40cb963fb
Hans Georg Schaathun <georg@schaathun.net>
2025-03-21 13:44:12 +0100

    docstring

--- src/ArM/Helper.hs
+++ src/ArM/Helper.hs
@@ -17,6 +17,9 @@ import Data.List (sort)
 import qualified Network.URI.Encode as URI
 import ArM.BasicIO
 
+-- |
+-- = Convenience functions for Maybe
+
 -- | Get a list from a maybe-list, mapping Nothing to the empty list.
 maybeList :: Maybe [a] -> [a]
 maybeList = fromMaybe []
@@ -38,6 +41,14 @@ filterNothing = f
           f (Nothing:xs) = f xs
           f (Just x:xs) = x:f xs
 
+-- | Skip Nothing values and apply the give function on Just-objects.
+passMaybe :: (Monad m) => (a -> m b) -> Maybe a -> m (Maybe b)
+passMaybe _ Nothing = return Nothing
+passMaybe g (Just x) = fmap Just $ g x
+
+-- |
+-- = Convenience display functions 
+
 maybeShow :: Show a => Maybe a -> String
 maybeShow Nothing = ""
 maybeShow (Just x) = show x
@@ -53,6 +64,9 @@ showSigned :: Int -> String
 showSigned x | x > 0 = "+" ++ show x
             | otherwise = show x
 
+-- |
+-- = Unique Sort
+
 -- | Sort the list and remove duplicates.
 uniqueSort :: (Ord a,Eq a) => [a] -> [a]
 uniqueSort = f . sort
@@ -61,10 +75,8 @@ uniqueSort = f . sort
           f (x:y:ys) | x == y = f (y:ys)
                      | otherwise = x:f (y:ys)
 
--- | Skip Nothing values and apply the give function on Just-objects.
-passMaybe :: (Monad m) => (a -> m b) -> Maybe a -> m (Maybe b)
-passMaybe _ Nothing = return Nothing
-passMaybe g (Just x) = fmap Just $ g x
+-- |
+-- = Convenience functions for Markdown
 
 -- | Set a markdown link, escaping spaces in the link.
 markdownLink :: String -> String -> String
